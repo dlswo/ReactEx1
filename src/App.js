2d0256d183ec806bcd34c08f4a6ba9cf2cdef0b0
dlswo <dlswo4456@naver.com>
2019-01-23 12:09:18 +0900

    commit!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './comp1/Counter'
+import SampleList from './comp1/SampleList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <SampleList/>
+          <Counter amount={1}></Counter>
+          <Counter amount={10}></Counter>
+          <Counter amount={100}></Counter>
+          <Counter amount={1000}></Counter>
       </div>
     );
   }
